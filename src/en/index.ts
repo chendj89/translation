5a6a742d2bd6f7b691fd1a189ba3af445f6fee6c
chendj89 <chendj89@hotmail.com>
2023-02-25 22:48:28 +0800

    优化代码

--- src/en/index.ts
+++ src/en/index.ts
@@ -12,6 +12,8 @@ import { en } from "../../package.json";
 export default async function index(root: string) {
   // 单词目录文件
   const list = path.join(root, en.list);
+  // ghpages
+  const _list = path.join(root, en.dist.root, en.dist.list);
   // 单词数据目录
   const dir = path.join(root, en.dist.root, en.dist.dir);
   // 确保文件一定存在
@@ -30,13 +32,14 @@ export default async function index(root: string) {
     listData = [];
   }
   const listDataArray = listData.map((item:typeof en.word.construction) => item.name);
- 
   // 确保单词数据目录存在
   fs.ensureDirSync(dir);
   // 读取所有单词数据文件路径 没有则是空数组
   const words = fs.readdirSync(dir);
   // 如果单词目录为空 那么放弃当前扫描任务
-  if (list.length == 0) {
+  if (listData.length == 0) {
+    fs.ensureFileSync(_list);
+    fs.writeFileSync(_list, '[]');
     return;
   }
   // 找出没有单词数据的单词
@@ -44,7 +47,6 @@ export default async function index(root: string) {
     listDataArray,
     words.map((item) => item.replace(".json", ""))
   );
-
   if (diff.length) {
     for await (const iterator of diff) {
       const data = await translation(iterator);
@@ -57,31 +59,10 @@ export default async function index(root: string) {
       fs.ensureFileSync(_wordPath);
       fs.writeFileSync(_wordPath, JSON.stringify(data, null, 2), "utf-8");
     }
-    const _list = path.join(root, en.dist.root, en.dist.list);
+    
     fs.ensureFileSync(_list);
     fs.writeFileSync(_list, JSON.stringify(listData, null, 2));
   }else {
     console.log("没有新单词添加")
-  }
-  // 需要删除的单词数据的单词
-  let diff2 = diffSet(
-    words.map((item) => item.replace(`.${en.dist.extname}`, "")),
-    listDataArray
-  );
-  // 删除不需要的单词数据的单词
-  if (diff2.length) {
-    for await (const iterator of diff2) {
-      const _wordPath = path.join(
-        root,
-        en.dist.root,
-        en.dist.dir,
-        `${iterator}.${en.dist.extname}`
-      );
-      if (fs.existsSync(_wordPath)) {
-        fs.unlinkSync(_wordPath);
-      }
-    }
-  }else {
-    console.log("没有单词被删除")
   }
 }
